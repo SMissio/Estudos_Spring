f9228586f966240f039bd2b935d743923451280b
Sylvia Missio <[200~sylvia.suzi.020@hotmail.com>
2022-03-31 18:59:41 -0300

    teste

--- src/main/java/org/example/projeto1/VendasApplication.java
+++ src/main/java/org/example/projeto1/VendasApplication.java
@@ -19,15 +19,38 @@ import java.util.List;
 @RestController
 public class VendasApplication {
     @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes){
+    public CommandLineRunner init(@Autowired Clientes clientes) {
         return args -> {
+
             clientes.salvar(new Cliente("Sylvia"));
             clientes.salvar(new Cliente("Outro Cliente"));
             List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
-        };
 
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado");
+                clientes.atualizar(c);
+            });
+
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("Cli").forEach(System.out::println);
+            //            System.out.println("deletando clientes");
+           // clientes.obterTodos().forEach(c -> {
+          //      clientes.deletar(c);
+          //  });
+            todosClientes = clientes.obterTodos();
+            if (todosClientes.isEmpty()) {
+                System.out.println("Nenhum cliente encontrado.");
+            } else {
+                todosClientes.forEach(System.out::println);
+            }
+        };
     }
+
 //    @Value("${application.name}")
 //    private String applicationName;
 
